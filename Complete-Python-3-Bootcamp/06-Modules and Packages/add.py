f95c05aed451a4f8ad30505cb358af6eb839cdcd
Rozi1 <rozi.kh2@gmail.com>
2024-05-13 14:54:43 +0500

    First Commit

---
+++ Complete-Python-3-Bootcamp/06-Modules and Packages/add.py
@@ -0,0 +1,6 @@
+
+print(5+10)
+
+from sample import add
+
+print(add(10, 4))
---
+++ Complete-Python-3-Bootcamp/06-Modules and Packages/main.py
@@ -0,0 +1,11 @@
+# import sample
+
+# print(sample.sample_variable)
+# print(sample.say_hello("Bilal"))
+# print(sample.add(2, 3))
+
+import sample as sp
+
+result = sp.add(5, 5)
+print(result)
+print(sp.say_hello("RK"))
